9ead19d20b41a8b3e197b34da192b00fd6dcafdb
BAN-m <dazedbj@gmail.com>
2018-03-14 16:28:22 -0400

    tests
    vue.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,7 +4,17 @@ const app = new Vue({
   el: '#app',
   data: {
     msg: 'hello!?',
-    appLogo: '/assets/logo.png',
+    isOnline: true,
+    query: '',
+    isPowerSyntaxEnabled: false,
+    searchIndices: [],
+  },
+  methods: {
+    executeSearch(token, event) {
+      const message = `Token: ${token} Query: ${this.query} Button: ${event.target.innerText}`;
+      // eslint-disable-next-line
+      alert(message);
+    },
   },
 });
 
